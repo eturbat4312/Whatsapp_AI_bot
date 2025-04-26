11cd39f2745a85b496264d9705314c1a9f73d19c
tory4312 <86555871+tory4312@users.noreply.github.com>
2025-04-26 23:29:32 +0200

    fix: remove env file from commit

--- whatsapp_ai_bot.py
+++ whatsapp_ai_bot.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import openai
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
